c74968e47d356d21c96776ec66d69c3827d83de0
Juliandazakipradipta <juliandazaki26@gmail.com>
2024-09-21 21:08:52 +0700

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const listmenu = document.getElementById('navbar-collapse-toggle')
+const btn = document.getElementById('button')
+
+btn.addEventListener('click', () => {
+    listmenu.style.display = 'block'
+})
