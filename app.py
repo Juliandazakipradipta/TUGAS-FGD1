ae7b450202b9a3879570273314c9157f684c10fa
Juliandazakipradipta <juliandazaki26@gmail.com>
2024-10-16 19:59:27 +0700

    commit lagi

--- app.py
+++ app.py
@@ -1,29 +1,11 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-
-client = MongoClient('mongodb+srv://test:sparta1@project0.wvnom.mongodb.net/')
-db = client.zaki
-
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/submit', methods=['POST'])
-def submit():
-    pesan_receive = request.form['pesan_give']
-    name = request.form['name']
-    email = request.form['email']
-    message = request.form['message']
-    
-    doc = {
-        'name': name,
-        'email': email,
-        'message': message,
-        'pesan' : pesan_receive
-    }
-    db.pesan.insert_one(doc) 
-    return jsonify({'msg': 'Saran & Kritik anda sudah masuk ke database saya!'})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=3000, debug=True)
--- static/script.js
+++
@@ -1,8 +0,0 @@
-const listmenu = document.getElementById('navbar-collapse-toggle')
-const btn = document.getElementById('button')
-
-btn.addEventListener('click', () => {
-    listmenu.style.display = 'block'
-})
-   
-
